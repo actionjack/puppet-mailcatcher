edcb7a55a7d756c3afbc9bfb48f933c7e87416ab
Martin Jackson <mjackson@equalexperts.com>
2013-10-23 10:34:18 +0100

    Added ruby and sqlite3 dependencies.

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,6 +1,9 @@
 # class mailcatcher::package
 #
 class mailcatcher::package {
+  package { ['ruby-dev','sqlite3','libsqlite3-dev']:
+    ensure => 'present'
+  } ->
   package { 'mailcatcher':
     ensure   => 'present',
     provider => 'gem'
