7da08e0bfb7d36c3a4d4fc00b6bc0bf1bb644ac3
Martin Jackson <mjackson@equalexperts.com>
2014-03-17 17:02:21 +0000

    Reverting params mailcatcher_spec and mailcater template

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,22 @@
 # class mailcatcher::params
 #
 class mailcatcher::params {
-
   $smtp_ip          = '0.0.0.0'
   $smtp_port        = '1025'
   $http_ip          = '0.0.0.0'
   $http_port        = '1080'
   $mailcatcher_path = '/usr/local/bin'
 
+  case $::osfamily {
+
+    'Debian': {
+      $packages = ['ruby-dev','sqlite3','libsqlite3-dev', 'rubygems']
+    }
+    'Redhat': {
+      fail("${::osfamily} is not supported yet.")
+    }
+    default: {
+      fail("${::osfamily} is not supported.")
+    }
+  }
 }
