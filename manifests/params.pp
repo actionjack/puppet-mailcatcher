f5866b252f472fe367c27fac03710a741a7070c2
Klaus <Klaus.Weidenbach@bestlife.ag>
2014-09-12 12:10:56 +0200

    fix milcatcher_path for CentOS
    On my CentOS6 box mailcatcher gets installed into /usr/bin by default with this puppet module.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,18 +5,19 @@ class mailcatcher::params {
   $smtp_port        = '1025'
   $http_ip          = '0.0.0.0'
   $http_port        = '1080'
-  $mailcatcher_path = '/usr/local/bin'
 
   case $::osfamily {
 
     'Debian': {
       $packages = ['ruby-dev','sqlite3','libsqlite3-dev', 'rubygems']
+      $mailcatcher_path = '/usr/local/bin'
       $config_file = '/etc/init/mailcatcher.conf'
       $template = 'mailcatcher/etc/init/mailcatcher.conf.erb'
       $provider = 'upstart'
      }
     'Redhat': {
       $packages = ['ruby-devel', 'sqlite-devel', 'rubygems', 'gcc-c++']      
+      $mailcatcher_path = '/usr/bin'
       $config_file = '/etc/init.d/mailcatcher'
       $template = 'mailcatcher/etc/init/mailcatcher.sysv.erb'
       $provider = 'redhat'
